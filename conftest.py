f52eb903a56c95227a588019cf2f0a4f45a08f47
MBorja <chealsy25@gmail.com>
2024-05-05 23:10:51 +0300

    Изменил значение scope

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="session")
 def browser_size():
     browser.config.window_height = 1080
     browser.config.window_width = 1920
