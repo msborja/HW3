a7695f7c01a7aacd21f29381aab15141771267bc
MBorja <chealsy25@gmail.com>
2024-05-05 17:56:07 +0300

    Добавил пустые строки для удобства чтения кода

--- test_HW3.py
+++ test_HW3.py
@@ -1,13 +1,16 @@
 
 from selene import browser, be, have
+
 def test_bing_success(browser_size):
     browser.open('https://www.bing.com/')
     (browser.element('[name="q"]').should(be.blank).type('Python 3.12 download').press_enter())
     browser.element('[class="b_algo"]').should(have.text('Python.org'))
+
 def test_duckduckgo_success(browser_size):
     browser.open('https://duckduckgo.com')
     (browser.element('[class="searchbox_input__bEGm3"]').should(be.blank).type('Python 3.12 download').press_enter())
     browser.element('[class="EKtkFWMYpwzMKOYr0GYm LQVY1Jpkk8nyJ6HBWKAk"]').should(have.text('Python.org'))
+
 def test_googlee_unsuccess(browser_size_v2):
     browser.open('https://google.com')
     (browser.element('[name="q"]').should(be.blank).type(
--- test_fixtures.py
+++ test_fixtures.py
@@ -15,10 +15,12 @@ def login_page(browser):
 def user():
     print("Юзер")
     return "username", "password"
+
 def test_login(login_page, user):
     username, password = user
     assert username == "username"
     assert password == "password"
+
 def test_logout(login_page, user):
     username, password = user
     assert username == "username"
