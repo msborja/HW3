46b12bd94b797b9890c8f7bd2d086101bf48a16b
MBorja <chealsy25@gmail.com>
2024-05-05 22:57:05 +0300

    Изменил используемую fixture в последнем тесте

--- test_HW3.py
+++ test_HW3.py
@@ -11,7 +11,7 @@ def test_duckduckgo_success(browser_size):
     (browser.element('[class="searchbox_input__bEGm3"]').should(be.blank).type('Python 3.12 download').press_enter())
     browser.element('[class="EKtkFWMYpwzMKOYr0GYm LQVY1Jpkk8nyJ6HBWKAk"]').should(have.text('Python.org'))
 
-def test_googlee_unsuccess(browser_size_v2):
+def test_googlee_unsuccess(browser_size):
     browser.open('https://google.com')
     (browser.element('[name="q"]').should(be.blank).type(
         'selene mod version 3.0.0 download torrent plz 123456789010').press_enter())
