2771c6f370a71dbe666f0822f267de12c50a883d
MBorja <chealsy25@gmail.com>
2024-05-05 22:56:12 +0300

    Удалил лишнюю fixture

--- conftest.py
+++ conftest.py
@@ -8,12 +8,5 @@ def browser_size():
     yield
     browser.quit()
 
-@pytest.fixture(scope="function")
-def browser_size_v2():
-    browser.config.window_height = 540
-    browser.config.window_width = 320
-    yield
-    browser.quit()
-
 
 
