082404df6fb739e30a4b29b273ddb62dec2b9b55
MBorja <chealsy25@gmail.com>
2024-05-05 17:52:37 +0300

    Добавил fixture_v2

--- conftest.py
+++ conftest.py
@@ -1,9 +1,19 @@
 import pytest
+from selene import browser
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def browser_size():
     browser.config.window_height = 1080
     browser.config.window_width = 1920
     yield
     browser.quit()
+
+@pytest.fixture(scope="function")
+def browser_size_v2():
+    browser.config.window_height = 540
+    browser.config.window_width = 320
+    yield
+    browser.quit()
+
+
 
